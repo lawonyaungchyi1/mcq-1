596aecd39fcfe7e53a5f1f9f77f5d6eb19202c54
lawonyaungchyi1 <nyanlintun225nlt@gmail.com>
2022-09-24 20:47:12 +0630

    First commit

---
+++ src/data/dummy.js
@@ -0,0 +1,224 @@
+export const zoology = [
+  {
+    id: 1,
+    questions: "Portal triad consists of the branches of",
+    selections: [
+      "Hepatic artery",
+      "Central vein",
+      "hepatic sinusoids ",
+      "Hepatic portal vein",
+      "bile ductule",
+    ],
+    correctAnswers: ["(a)", "(d)", "(e)"],
+  },
+  {
+    id: 2,
+    questions:
+      "Which of the following nutrients is/are correctly matched with its/their utility?",
+    selections: [
+      "carbohydrates         ---energy yielding",
+      "fat       ---body building",
+      "protein     ----growth and repair",
+      "vitamins      ----maintenance of the body",
+      "Nucleic acids     -----Hereditary substance",
+    ],
+    correctAnswers: ["(a)", "(c)", "(d)", "(e)"],
+  },
+  {
+    id: 3,
+    questions: "Which of the following/s is/are functions of hormone gastrin?",
+    selections: [
+      "Control the contraction of pyloric sphincter",
+      "Stimulates the secretion of the gastric juice",
+      "Serves in protein synthesis",
+      "Stops salivary enzyme action",
+      "Inhibits the secretion of the gastric juice",
+    ],
+    correctAnswers: ["(a)", "(b)"],
+  },
+  {
+    id: 4,
+    questions: "Which of the following is/are true?",
+    selections: [
+      "The fat globules encased in a protein coat are called chylomicrons",
+      "Lacterals conduct chylomicrons into larger blood vessels",
+      "The amino acids and monosaccharides first coming into the liver through Portal circulation",
+      "Glycogenolysis is the converting the glucose into glycogen",
+      "Absorption of water , glucose , alcohol and some drugs occur in the intestine of man.",
+    ],
+    correctAnswers: ["(a)", "(c)"],
+  },
+  {
+    id: 5,
+    questions: "Which of the following is/are true?",
+    selections: [
+      "No digestive enzyme in oesophagus",
+      "The secretion of the gastric juice is inhibited by enterogasterone",
+      "A portal traid consist hepatic portal vein , hepatic artery and bile ductule.",
+      "Succus entericus is released from crypts of Lieberkuhn.",
+      "Large intestine absorb water.",
+    ],
+    correctAnswers: ["(a)", "(b)", "(c)", "(d)", "(e)"],
+  },
+  {
+    id: 6,
+    questions: "Thick wall of stomach have",
+    selections: [
+      "Several vascular villi",
+      "Spiral internal folds",
+      "Longitudinal fold",
+      "Longitudinal muscular cord",
+      "Small pouch-like dilation",
+    ],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 7,
+    questions:
+      "Which of the following statement(s) is/are true or false for oesophagus",
+    selections: [
+      "It is long due to the presence of neck",
+      "It opens into the stomach by pyloric orifice",
+      "Its wall is thick and muscular",
+      "Its lumen extends only when food arrives in it",
+      "Its wall have a few longitudinal folds",
+    ],
+    correctAnswers: ["(a)", "(c)", "(d)"],
+  },
+  {
+    id: 8,
+    questions: "Tongue helps",
+    selections: [
+      "in absorption",
+      "in ingestion",
+      "in digestion",
+      "in deglutition",
+      "in assimilation",
+    ],
+    correctAnswers: ["(b)", "(c)", "(d)"],
+  },
+  {
+    id: 9,
+    questions: "Which of the following/s is/are true?",
+    selections: [
+      "HCL control the contraction of pyloric sphincter.",
+      "Hormone gastrin stimulates the secretion of the gastric juice",
+      "HCL serves in protein synthesis",
+      "No chemical digestion in the oesophagus",
+      "Chemical digestion begins in the mouth and ends in the large intestine",
+    ],
+    correctAnswers: ["(a)", "(c)", "(d)"],
+  },
+  {
+    id: 10,
+    questions: "Which of the following/s is/are function/s of the liver?",
+    selections: [
+      "activate pepsinogen",
+      "regulates pyloric sphincter",
+      "forms antianemic factor aiding in red blood cells",
+      "produce heparin",
+      "stores vitamins",
+    ],
+    correctAnswers: ["(c)", "(d)", "(e)"],
+  },
+  {
+    id: 11,
+    questions: "Which of the following/s is/are the covering/s of the liver?",
+    selections: [
+      "pleural membrane",
+      "pericardial membrane",
+      "serous capsule",
+      "schneiderian membrane",
+      "Glisson's capsule",
+    ],
+    correctAnswers: ["(c)", "(e)"],
+  },
+  {
+    id: 12,
+    questions: "Which of the following/s is are function/s of HCL?",
+    selections: [
+      "activate pepsinogen",
+      "regulates pyloric sphincter ",
+      "forms antianemic factor aiding in red blood cells",
+      "kills harmful bacteria",
+      "splits nucleoproteins",
+    ],
+    correctAnswers: ["(a)", "(b)", "(d)", "(e)"],
+  },
+  {
+    id: 13,
+    questions: "Which of the following/s is/are function/s of bile?",
+    selections: [
+      "intensification of intestinal contractions",
+      "regulates pyloric sphincter",
+      "forms antianemic factor aiding in red blood cells",
+      "easy absorption of fatty acids , cholesterol and other lipids by forming micelles.",
+      "neutralization of acidity of HCL",
+    ],
+    correctAnswers: ["(a)", "(d)", "(e)"],
+  },
+  {
+    id: 14,
+    questions:
+      "The separation of nasal passage from buccopharyngeal cavity is/are?",
+    selections: [
+      "hard palate",
+      " soft palate",
+      "velum palate",
+      "turbinal",
+      " tongue",
+    ],
+    correctAnswers: ["(a)", "(b)", "(c)"],
+  },
+  {
+    id: 15,
+    questions: "Which of the following/s is are true?",
+    selections: [
+      "Chylomicrons make their way out of absorptive cells as chyme and enter lacteral",
+      "The fats are converted into amino acids and carbohydrates by the kidney ",
+      "Animal foods form large amount of faeces than vegetable foods",
+      "The amino acids are constantly needed for the formation of protoplasm.",
+      "No chemical digestion in the large intestine.",
+    ],
+    correctAnswers: ["(d)", "(e)"],
+  },
+  {
+    id: 16,
+    questions:
+      "In the typical general pattern of the gut wall, muscularis externa consists of",
+    selections: [
+      "circular muscle",
+      "longitudinal muscle",
+      "blood vessels",
+      " surface epithelium of columnar type",
+      " loose connective tissue with glands",
+    ],
+    correctAnswers: ["(a)", "(b)", "(c)"],
+  },
+  {
+    id: 17,
+    questions: "Which of the following/s is/are true?",
+    selections: [
+      "The basic source of energy for all animals is carbohydrates.",
+      "Animals obtain their carbohydrates from the external environment.",
+      "Other useful carbohydrate is maltase.",
+      "The carbohydrate is metabolized during cellular respiration. ",
+      "About one third to two third of the total calories consumes daily are from Carbohydrates.",
+    ],
+    correctAnswers: ["(a)", "(b)", "(d)"],
+  },
+
+  {
+    id: 18,
+    questions:
+      "Which of the following/s is/are the soluble form/s of the given nutrition?",
+    selections: [
+      "carbohydrate     -----glucose",
+      "protein     ----amino acid",
+      "fat      -----fatty acid and glycerol",
+      "Nucleic acid     -----Nucleotides",
+      "Starch    ----fructose",
+    ],
+    correctAnswers: ["(a)", "(b)", "(c)", "(d)", "(e)"],
+  },
+];
